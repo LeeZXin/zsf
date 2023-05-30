847c59ac3c762f67c3b456433383d09cea665e09
LeeZXin <48628959+LeeZXin@users.noreply.github.com>
2023-05-30 14:17:04 +0800

    添加规则引擎流程编排功能

---
+++ zengine/handlers.go
@@ -0,0 +1,33 @@
+package zengine
+
+import (
+	lua "github.com/yuin/gopher-lua"
+)
+
+// ScriptHandler 脚本执行节点
+type ScriptHandler struct {
+}
+
+func (*ScriptHandler) GetName() string {
+	return "scriptNode"
+}
+
+func (*ScriptHandler) Do(params *InputParams, bindings Bindings, luaExecutor *ScriptExecutor) (Bindings, error) {
+	output := make(Bindings)
+	script, err := params.GetCompiledScript(luaExecutor)
+	if err != nil {
+		return output, err
+	}
+	scriptRet, err := luaExecutor.Execute(script, bindings)
+	if err != nil {
+		return output, err
+	}
+  // 对脚本返回值进行处理
+	if scriptRet.Type() == lua.LTTable {
+		m, ok := ToGoValue(scriptRet).(map[string]any)
+		if ok {
+			output.PutAll(m)
+		}
+	}
+	return output, nil
+}
