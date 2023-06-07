0e4f5f3ea39ca8a50325ab32b239d5c63437a288
LeeZXin <LeeZXin>
2023-06-07 21:35:29 +0800

    feat: default consul address

--- consul/consul.go
+++ consul/consul.go
@@ -8,10 +8,7 @@ import (
 
 func NewConsulClient(consulAddr, consulToken string) *api.Client {
 	if consulAddr == "" {
-		logger.Logger.Panic("empty consul addr")
-	}
-	if consulToken == "" {
-		logger.Logger.Panic("empty consul token")
+		consulAddr = "127.0.0.1:8500"
 	}
 	defaultConfig := api.DefaultConfig()
 	defaultConfig.Address = consulAddr
