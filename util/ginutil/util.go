e46b1ce9d73c469fa5a89ee6284ad05c8f54120b
LeeZXin <LeeZXin>
2023-07-23 23:45:28 +0800

    feat: ginutil

--- util/ginutil/util.go
+++ util/ginutil/util.go
@@ -42,3 +42,11 @@ func BindJson(obj any, c *gin.Context) bool {
 	}
 	return true
 }
+
+func GetClientIp(c *gin.Context) string {
+	ip := c.ClientIP()
+	if ip == "::1" {
+		return "127.0.0.1"
+	}
+	return ip
+}
