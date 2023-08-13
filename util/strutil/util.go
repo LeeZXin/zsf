e81a6caedd3d6ec6d2a3074d82fc8cb651ef6a0d
LeeZXin <LeeZXin>
2023-08-13 13:20:28 +0800

    feat: strutil

--- util/strutil/util.go
+++ util/strutil/util.go
@@ -1,6 +1,8 @@
 package strutil
 
 import (
+	"fmt"
+	"math/rand"
 	"strings"
 )
 
@@ -20,4 +22,30 @@ func To62Str(i uint64) string {
 		i /= 62
 	}
 	return sb.String()
+}
+
+// RandomStr 指定长度随机字符串
+func RandomStr(length int) string {
+	if length <= 0 {
+		return ""
+	}
+	sb := strings.Builder{}
+	for i := 0; i < length; i++ {
+		sb.WriteString(c62[rand.Intn(62)])
+	}
+	return sb.String()
+}
+
+func Concat(data []any, delimiter string) string {
+	if data == nil || len(data) == 0 {
+		return ""
+	}
+	ret := strings.Builder{}
+	for i, d := range data {
+		ret.WriteString(fmt.Sprintf("%v", d))
+		if i < len(data)-1 {
+			ret.WriteString(delimiter)
+		}
+	}
+	return ret.String()
 }
