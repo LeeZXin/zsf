a66e23bba9812a124a2ba3295a5f17ebccde9bc6
LeeZXin <LeeZXin>
2023-11-02 22:27:08 +0800

    feat: customize

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import (
 	_ "github.com/LeeZXin/zsf/grpc/grpcserver"
 	_ "github.com/LeeZXin/zsf/http/httpserver"
-	"github.com/LeeZXin/zsf/property/dynamic"
 	"github.com/LeeZXin/zsf/zsf"
 )
 
 func main() {
-	dynamic.GetString("string")
 	zsf.Run()
 }
