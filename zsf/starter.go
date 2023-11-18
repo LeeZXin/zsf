a71b8bbea57f40e995e06797927265087bca2f2a
LeeZXin <LeeZXin>
2023-11-18 17:17:22 +0800

    feat: new features

--- zsf/starter.go
+++ zsf/starter.go
@@ -3,8 +3,14 @@ package zsf
 import (
 	"fmt"
 	"github.com/LeeZXin/zsf-utils/quit"
+	"github.com/LeeZXin/zsf/common"
+	"github.com/LeeZXin/zsf/logger"
 	sentinel "github.com/alibaba/sentinel-golang/api"
+	"os"
+	"path/filepath"
+	"strconv"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -12,6 +18,10 @@ var (
 	startOnce sync.Once
 	// 服务启动时间
 	startTime = time.Now()
+	// 启动模式
+	runMode = atomic.Value{}
+	// 版本
+	version = atomic.Value{}
 )
 
 func init() {
@@ -20,6 +30,22 @@ func init() {
 
 func GetStartTime() time.Time {
 	return startTime
+}
+
+func GetRunMode() string {
+	val := runMode.Load()
+	if val == nil {
+		return ""
+	}
+	return val.(string)
+}
+
+func GetVersion() string {
+	val := version.Load()
+	if val == nil {
+		return ""
+	}
+	return val.(string)
 }
 
 func Run(options ...Option) {
@@ -43,6 +69,14 @@ func Run(options ...Option) {
 :: zsf :: 
 `)
 		}
+		if o.Version != "" {
+			version.Store(o.Version)
+			logger.Logger.Info(fmt.Sprintf("start %s with version = %s ::", common.GetApplicationName(), o.Version))
+		}
+		if o.PidPath != "" {
+			createPidFile(o.PidPath)
+		}
+		runMode.Store(o.RunMode)
 		onApplicationStart()
 		quit.AddShutdownHook(func() {
 			onApplicationShutdown()
@@ -53,7 +87,10 @@ func Run(options ...Option) {
 }
 
 type option struct {
-	Banner string
+	Banner  string
+	Version string
+	PidPath string
+	RunMode string
 }
 
 type Option func(*option)
@@ -61,5 +98,39 @@ type Option func(*option)
 func WithBanner(banner string) Option {
 	return func(o *option) {
 		o.Banner = banner
+	}
+}
+
+func WithVersion(version string) Option {
+	return func(o *option) {
+		o.Version = version
+	}
+}
+
+func WithPidFile(filePath string) Option {
+	return func(o *option) {
+		o.PidPath = filePath
+	}
+}
+
+func WithRunMode(runMode string) Option {
+	return func(o *option) {
+		o.RunMode = runMode
+	}
+}
+
+func createPidFile(filePath string) {
+	currentPid := os.Getpid()
+	if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+		logger.Logger.Panic("create PID folder: %v", err)
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		logger.Logger.Panic("create PID file: %v", err)
+	}
+	defer file.Close()
+	if _, err := file.WriteString(strconv.FormatInt(int64(currentPid), 10)); err != nil {
+		logger.Logger.Panic("write PID information: %v", err)
 	}
 }
