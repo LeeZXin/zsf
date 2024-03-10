42115fc679b409b02eb4e06b2b3b4f189a0e5de2
LeeZXin <LeeZXin>
2024-03-10 14:42:21 +0800

    feat: http task

--- http/httptask/task.go
+++ http/httptask/task.go
@@ -1,48 +1,46 @@
 package httptask
 
 import (
-	"context"
 	"github.com/LeeZXin/zsf-utils/threadutil"
 	"github.com/LeeZXin/zsf/http/httpserver"
 	"github.com/LeeZXin/zsf/logger"
-	"github.com/LeeZXin/zsf/property/static"
 	"github.com/gin-gonic/gin"
+	"io"
 	"net/http"
 	"net/url"
-	"sync"
 )
 
-type HttpTask func(context.Context, map[string]any, url.Values)
+type HttpTask func(io.Reader, url.Values)
 
 var (
-	taskMap = sync.Map{}
-	token   = static.GetString("httptask.token")
+	taskMap = make(map[string]HttpTask, 8)
 )
 
+// AppendHttpTask 添加http task not thread-safe
 func AppendHttpTask(name string, task HttpTask) {
 	if task == nil {
 		return
 	}
-	taskMap.Store(name, task)
+	_, b := taskMap[name]
+	if b {
+		logger.Logger.Fatalf("duplicated http task name: %s", name)
+	}
+	taskMap[name] = task
 }
 
 func init() {
 	httpserver.AppendRegisterRouterFunc(func(e *gin.Engine) {
 		e.Any("/httpTask/v1/:taskName", func(c *gin.Context) {
-			if token == "" || c.Request.Header.Get("task-token") != token {
-				c.String(http.StatusForbidden, "forbidden")
-				return
-			}
 			taskName := c.Param("taskName")
-			task, b := taskMap.Load(taskName)
+			task, b := taskMap[taskName]
 			if !b {
 				c.String(http.StatusNotFound, "task not found")
 			} else {
-				body := make(map[string]any, 8)
-				c.ShouldBind(&body)
+				body := c.Request.Body
+				defer body.Close()
 				go func() {
 					if err := threadutil.RunSafe(func() {
-						task.(HttpTask)(c.Request.Context(), body, c.Request.URL.Query())
+						task(body, c.Request.URL.Query())
 					}); err != nil {
 						logger.Logger.Errorf("httpTask: %s err: %s", taskName, err)
 					}
