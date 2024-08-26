c8f47dd9b439527ca12668b8e353feca714c89bf
LeeZXin <LeeZXin>
2024-08-26 11:43:22 +0800

    feat: shrink loki labels

--- main.go
+++ main.go
@@ -1,43 +1,31 @@
 package main
 
 import (
-	"github.com/LeeZXin/zsf-utils/idutil"
-	"github.com/LeeZXin/zsf/logger"
+	"github.com/LeeZXin/zsf/actuator"
+	"github.com/LeeZXin/zsf/http/httpserver"
+	"github.com/LeeZXin/zsf/pprof"
+	"github.com/LeeZXin/zsf/prom"
+	"github.com/LeeZXin/zsf/property/dynamic"
+	"github.com/LeeZXin/zsf/services/discovery"
+	"github.com/LeeZXin/zsf/services/registry"
 	"github.com/LeeZXin/zsf/zsf"
 )
 
 func main() {
-	//dynamic.Init()
-	//zsf.Run(
-	//	zsf.WithDiscovery(discovery.NewEtcdDiscovery()),
-	//	zsf.WithLifeCycles(
-	//		httpserver.NewServer(
-	//			httpserver.WithRegistryAction(
-	//				registry.NewDefaultHttpAction(
-	//					registry.NewDefaultEtcdRegistry(),
-	//				),
-	//			),
-	//		),
-	//		actuator.NewServer(),
-	//		prom.NewServer(),
-	//		pprof.NewServer(),
-	//	),
-	//)
-	go func() {
-		for k := 0; k < 10000; k++ {
-			logger.Logger.Info(idutil.RandomUuid())
-		}
-	}()
-	go func() {
-		for k := 0; k < 10000; k++ {
-			logger.Logger.Error(idutil.RandomUuid())
-		}
-	}()
-	go func() {
-		for k := 0; k < 10000; k++ {
-			logger.Logger.Warn(idutil.RandomUuid())
-		}
-	}()
-
-	zsf.Run()
+	dynamic.Init()
+	zsf.Run(
+		zsf.WithDiscovery(discovery.NewEtcdDiscovery()),
+		zsf.WithLifeCycles(
+			httpserver.NewServer(
+				httpserver.WithRegistryAction(
+					registry.NewDefaultHttpAction(
+						registry.NewDefaultEtcdRegistry(),
+					),
+				),
+			),
+			actuator.NewServer(),
+			prom.NewServer(),
+			pprof.NewServer(),
+		),
+	)
 }
